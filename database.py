aa1bda9aa7bf632baddbc141ffa7d70f064ea90a
kissvivien <kiss.vivien@diak.szbi-pg.hu>
2025-03-28 13:21:43 +0100

    KOMMENTEK HOZAADASA

--- database.py
+++ database.py
@@ -8,11 +8,25 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+#ADATBÁZIS LÉTREHOZÁSA
 DATABASE = "mydatabase"
 mycursor.execute(F"CREATE DATABASE IF NOT EXISTS {DATABASE}")
+
+# ADATBÁZISOK MUTATÁSA
 mycursor.execute("SHOW DATABASES")
 
 for x in mycursor:
   print(x)
 
-mycursor.execute(F"USE {DATABASE}")  
+#HASZNÁLD EZT AZ ADATBÁZIST
+mycursor.execute(F"USE {DATABASE}") 
+
+#CUSTOMERS TÁBLA LÉTREHOZÁSA
+mycursor.execute("CREATE TABLE IF NOT EXISTS customers (name VARCHAR(255), address VARCHAR(255))")
+P("-------")
+
+#TÁBLÁK MUTATÁSA
+mycursor.execute("SHOW TABLES ")
+
+for x in mycursor:
+  print(x)
