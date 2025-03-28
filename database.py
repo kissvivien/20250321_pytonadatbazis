080bc0d61c6fbc433e10d061b4aa646ca1c17040
kissvivien <kiss.vivien@diak.szbi-pg.hu>
2025-03-28 13:09:49 +0100

    SHOW DATA

--- database.py
+++ database.py
@@ -1,11 +1,18 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="localhost",
+  host="127.0.0.1",
   user="root",
-  password=""
+  password="mysql"
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE mydatabase")
+DATABASE = "mydatabase"
+mycursor.execute(F"CREATE DATABASE IF NOT EXISTS {DATABASE}")
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  print(x)
+
+mycursor.execute(F"USE {DATABASE}")  
